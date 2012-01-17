a8acab5cd4d4b1cb2d2b4f140bb4c68e653f8de2
Ken Wronkiewicz <ken.wronkiewicz@cloudkick.com>
2012-01-17 15:36:24 -0800

    working for reals now.
    Conflicts:
    
    	lib/thrift/connection.js

--- test/reconnect.js
+++ test/reconnect.js
@@ -5,12 +5,14 @@ ttypes = require('./generated/gen-nodejs/ThriftTest_types');
 Int64 = require('node-int64');
 var async = require('async');
 
+var blahstr = '';
 
 var server = thrift.createServer(ThriftTest, {
   testVoid: function(success) {
     success();
   },
   testString: function(thing, success) {
+    blahstr = blahstr + thing;
     success(thing);
   },
   testOneway: function(secondsToSleep) {
@@ -19,66 +21,13 @@ var server = thrift.createServer(ThriftTest, {
 });
 
 exports['test_reconnect_reopen'] = function(test, assert) {
-  server.listen(9162);
-  async.series([
-    function(callback) {
-      var connection = thrift.createConnection('localhost', 9162),
-      client = thrift.createClient(ThriftTest, connection);
-      
-      client.testVoid(function(err, response) {
-        assert.ifError(err);
-        connection.end();
-        server.close();
-        callback();
-      });
-    },
-    function(callback) {
-      server.listen(9162);
-      var connection = thrift.createConnection('localhost', 9162),
-      client = thrift.createClient(ThriftTest, connection);
-      
-      client.testVoid(function(err, response) {
-        assert.ifError(err);
-        connection.end();
-        callback();
-      });
-    }
-  ], function(err) {
-    assert.ifError(err);
-    server.close();
-    test.finish();
-  });
-}
-
-exports['test_disconnect_detect'] = function(test, assert) {
-  server.listen(9162);
-  var connection = thrift.createConnection('localhost', 9162); 
-  var conncheck = false;
-
-  connection.on('end', function() {
-    conncheck = true;
-    connection.end();
-    test.finish();
-  });
-  
-  var client = thrift.createClient(ThriftTest, connection);
-  client.testVoid(function(err, response) {
-    assert.ifError(err);
-    server.close();
-  });
-}
-
-exports['test_reconnect'] = function(test, assert) {
   test.skip();
   server.listen(9162);
-  var connection = thrift.createConnection('localhost', 9162, {autorestart: true}); 
-  var conncheck = false;
-  connection.once('close', function() {
-    conncheck = true;
-  });
   async.series([
     function(callback) {
-      var client = thrift.createClient(ThriftTest, connection);
+      var connection = thrift.createConnection('localhost', 9162),
+      client = thrift.createClient(ThriftTest, connection);
+      
       client.testVoid(function(err, response) {
         assert.ifError(err);
         connection.end();
@@ -88,7 +37,9 @@ exports['test_reconnect'] = function(test, assert) {
     },
     function(callback) {
       server.listen(9162);
-      var client = thrift.createClient(ThriftTest, connection);
+      var connection = thrift.createConnection('localhost', 9162),
+      client = thrift.createClient(ThriftTest, connection);
+      
       client.testVoid(function(err, response) {
         assert.ifError(err);
         connection.end();
@@ -97,6 +48,56 @@ exports['test_reconnect'] = function(test, assert) {
     }
   ], function(err) {
     assert.ifError(err);
+    server.close();
+    test.finish();
+  });
+}
+
+
+exports['test_reconnect'] = function(test, assert) {
+  server.listen(9162);
+  var connection = thrift.createConnection('localhost', 9162, {autorestart: true}); 
+  var client = thrift.createClient(ThriftTest, connection);
+  var conncheck = false;
+  connection.once('close', function() {
+    
+    conncheck = true;
+  });
+  async.series([
+    function(callback) {
+      client.testString('dead', function(err, response) {
+        assert.deepEqual(response,'dead');
+        assert.ifError(err);
+        connection.connection.destroy();
+        callback();
+      });
+    },
+    function(callback) {
+      client.testString('cow', function(err, response) {
+        assert.deepEqual(response,'cow');
+        assert.ifError(err);
+        callback();
+      });
+    },
+    function(callback) {
+      client.testString('cat', function(err, response) {
+        assert.deepEqual(response,'cat');
+        assert.ifError(err);
+        connection.connection.destroy();
+        callback();
+      });
+    },
+    function(callback) {
+      client.testString('dog', function(err, response) {
+        assert.deepEqual(response,'dog');
+        assert.ifError(err);
+        callback();
+      });
+    }
+  ], function(err) {
+    assert.deepEqual('deadcowcatdog', blahstr);
+    assert.ifError(err);
+    connection.end();
     server.close();
     test.finish();
   });
